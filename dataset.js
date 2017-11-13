217c229d1b82719ed66f1f5883414e4dedfc6d87
Sarath <sarath.hacks@gmail.com>
2017-11-13 15:45:09 +0530

    :cake: B'Day updates

--- dataset.js
+++ dataset.js
@@ -17,7 +17,107 @@ var bds = [
                 name:"Amrithlal"
             }
         ]
+    },
+    {
+        event:"birthday",
+        date: "20-8",
+        peoples: [
+            { name: "Akhil"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "19-5",
+        peoples: [
+            { name: "Jobin"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "28-7",
+        peoples: [
+            { name: "Dheerej"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "28-11",
+        peoples: [
+            { name: "Suman"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "22-12",
+        peoples: [
+            { name: "Roopesh"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "6-10",
+        peoples: [
+            { name: "Shameem"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "30-10",
+        peoples: [
+            { name: "Sarath NK"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "30-5",
+        peoples: [
+            { name: "Sarath TK"},
+            { name: "Ratheesh"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "22-8",
+        peoples: [
+            { name: "Prasanth"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "2-3",
+        peoples: [
+            { name: "Subin"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "6-10",
+        peoples: [
+            { name: "Anoop"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "25-2",
+        peoples: [
+            { name: "Dalton"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "20-11",
+        peoples: [
+            { name: "Anu"}
+        ]
+    },
+    {
+        event:"birthday",
+        date: "3-7",
+        peoples: [
+            { name: "Jojo"}
+        ]
     }
+    
 ]
 
 var quotes = [
